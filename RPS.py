d1932cf3ae9e82deebb9ff47578b3f3ed00ec25f
Matias Nahuelhual <matiasnahuelhual0@gmail.com>
2024-05-12 13:06:29 +0000

    update

--- RPS.py
+++ RPS.py
@@ -1,10 +1,11 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# La función de ejemplo siguiente realiza un seguimiento del historial del oponente y reproduce lo que el oponente jugó hace dos jugadas. 
+# No es un muy buen jugador, por lo que deberás cambiar el código para superar el desafío.
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) > 1:
+        guess = opponent_history[-1]
 
     return guess
